7fe43b7380fb15b378c635dfcee8d6650d1ebd9a
mroosmalen <m.vanroosmalen-2@umcutrecht.nl>
2019-07-01 07:49:42 +0200

    Upgrade version number

--- vaf_checker.py
+++ vaf_checker.py
@@ -15,7 +15,7 @@ import glob
 
 # Get version from git
 #__version__ = subprocess.check_output(["git", "describe"]).strip().decode('UTF-8')
-__version__ = 'v.1.1.0'
+__version__ = 'v1.1.1'
 
 # Set arguments
 parser = argparse.ArgumentParser()
